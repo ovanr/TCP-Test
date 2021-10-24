6812602953c2d96af4d7a2987cf0214d2175c1b5
David Oberacker <david.oberacker@gmail.com>
2021-10-24 15:39:46 +0200

    Add baseTestCase.py module and add skeleton for the testRunner.py class.

--- testRunner.py
+++ testRunner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import List
 
 import asyncio
 from threading import Thread
@@ -6,7 +7,36 @@ from threading import Thread
 import jsonpickle
 import websockets
 
+from testCommand import TestCommand
 from config import TEST_RUNNER_PORT
+
+
+class TestRunner:
+    __serverQueue = []
+    __sutQueue = []
+
+    @property
+    def server_queue(self) -> List[TestCommand]:
+        return self.__serverQueue
+
+    @server_queue.setter
+    def server_queue(self, queue: List[TestCommand]):
+        self.__serverQueue.append(queue)
+
+    @property
+    def sut_queue(self) -> List[TestCommand]:
+        return self.__sutQueue
+
+    @sut_queue.setter
+    def sut_queue(self, queue: List[TestCommand]):
+        self.__sutQueue.append(queue)
+
+    def run(self):
+        pass
+
+    def cleanup(self):
+        pass
+
 
 serverQueue = []
 sutQueue = []
